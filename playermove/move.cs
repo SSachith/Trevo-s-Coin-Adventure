4b30b858273604f1922f1b83bd1da66d3c437ff8
Sachith <SSachith@users.noreply.github.com>
2018-11-21 17:37:27 +0530

    uploading

---
+++ playermove/move.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour {
+    public CharacterController2D controller;
+    float LRmove = 0f;
+    public float runspeed = 10f;
+    bool jump = false;
+
+	// Use this for initialization
+
+	
+	// Update is called once per frame
+	void Update () {
+        LRmove = Input.GetAxisRaw("Horizontal") * runspeed;
+        if (Input.GetButtonDown("Jump")) {
+            jump = true;
+        }
+	}
+
+    void FixedUpdate() {
+
+        controller.Move(LRmove * Time.fixedDeltaTime,false,jump);
+        jump = false;
+    }
+
+
+}
