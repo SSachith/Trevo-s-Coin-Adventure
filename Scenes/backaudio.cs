84421db641ecd3ef9efa5bb7c4ca3252caff9e08
Sachith <SSachith@users.noreply.github.com>
2018-11-21 17:46:27 +0530

    upload

---
+++ Scenes/backaudio.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backaudio : MonoBehaviour {
+    
+    
+    public AudioClip background1;
+    public AudioClip background0;
+    private AudioSource source;
+	
+    
+    
+  //  void Awake() {
+   //     source = GetComponent<AudioSource>();
+		
+	//}
+	
+	// Update is called once per frame
+	//void FixedUpdate () {
+
+        //if (!source.isPlaying)
+          //  {
+               // vol = Random.Range(vollrange, volhrange);
+                //source.pitch = Random.Range(0.9f, 1.5f);
+                //source.PlayOneShot(footstep, vol);
+           // }
+       // }
+       // else
+       // {
+       //     source.Stop();
+       }
+		
+	
